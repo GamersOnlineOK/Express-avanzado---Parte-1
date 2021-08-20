5d0e8d991ec6e2af16c31a0a7fca62f86ab55082
Leonardo De Los Santos <leo_dls@hotmail.com>
2021-08-19 23:07:50 -0300

    9. Express avanzado - Parte 2

--- index.js
+++ index.js
@@ -1,64 +1,20 @@
 const exp = require('constants');
-const express = require('express')
+const express = require('express');
 const App = express();
+App.use(express.static('public'));
+const productos=require('./rutas/productos.route');
+
 App.use(express.json());
 App.use(express.urlencoded({ extended: false }));
-const fs = require("fs");
+
 const Port = 8080;
-const FILE_PRODUCTOS = "productos.txt";
+
+
+App.use('/api',productos);
 // =========== LEVANTAR SERVIDOR==========
 App.listen(Port, () => console.log(`Servidor Corriendo en el puerto ${Port} `))
 // =========== CORRIENDO APLICACION=======
 
-// METODOS GET
-// ==============LISTAR PRODUCTO===================
-App.get('/api/productos/listar', (req, res) => {
-    var productos = fs.promises.readFile(FILE_PRODUCTOS)
-        .then(data => data.toString('utf-8'))
-        .then(datos => {
-            const json = JSON.parse(datos);
-            {json.length>0?(res.json({ item: json, cantidad: json.length })):(res.send("no hay productos en la lista"))}
-            
-        })
-})
-// ==============MOSTRAR PRODUCTO===================
-App.get('/api/productos/listar/:id', (req, res) => {
-    let parametro = req.params.id;
-    fs.promises.readFile('./productos.txt')
-        .then(data => data.toString('utf-8'))
-        .then(datos => {
-            const json = JSON.parse(datos);
-            const filter=json.filter(data=>data.id==parametro);
-            { filter.length>0 ? (res.json({ filter })) : (res.json({ error: 'producto no encontrado' })) }
 
-        })
-})
-// ==============GUARDAR PRODUCTO===================
-App.post('/api/productos/guardar', (req, res) => {
-    
-    const array = 
-        {
-            titulo: req.body.titulo,
-            price: req.body.price,
-            img: req.body.img,
-            
-        };
-        
-       
-        fs.promises.readFile(FILE_PRODUCTOS).then(data => {
 
-            const json = JSON.parse(data.toString('utf-8'));
-            const producto=({...array, id:json.length +1});
-            const productoFinal=json.push(producto);
-            res.send(producto)
-            fs.promises.writeFile(FILE_PRODUCTOS, JSON.stringify(json, null, "\t"))
-            .then(() => {
-                console.log("Producto Agregado Correctamente");
-            })
-        }).then(data=>{
-            fs.promises.readFile(FILE_PRODUCTOS)
-            .then(data=>JSON.parse(data.toString('utf-8')))
-            .then(data=>res.json(data.length))
-            
-        })        
-});
+
---
+++ rutas/productos.route.js
@@ -0,0 +1,101 @@
+const express=require("express");
+const App = express();
+const router=express.Router();
+const fs = require("fs");
+App.use(express.static('public'));
+const FILE_PRODUCTOS = "productos.txt";
+
+// METODOS GET
+// ==============LISTAR PRODUCTO===================
+router.get('/productos/listar', (req, res) => {
+    var productos = fs.promises.readFile(FILE_PRODUCTOS)
+        .then(data => data.toString('utf-8'))
+        .then(datos => {
+            const json = JSON.parse(datos);
+            {json.length>0?(res.json({ item: json, cantidad: json.length })):(res.send("no hay productos en la lista"))}
+            
+        })
+})
+// ==============MOSTRAR PRODUCTO===================
+router.get('/productos/listar/:id', (req, res) => {
+    let parametro = req.params.id;
+    fs.promises.readFile('./productos.txt')
+        .then(data => data.toString('utf-8'))
+        .then(datos => {
+            const json = JSON.parse(datos);
+            const filter=json.filter(data=>data.id==parametro);
+            { filter.length>0 ? (res.json({ filter })) : (res.json({ error: 'producto no encontrado' })) }
+
+        })
+})
+// METODOS POST
+// ==============GUARDAR PRODUCTO===================
+router.post('/productos/guardar', (req, res) => {
+    
+    const array = 
+        {
+            titulo: req.body.titulo,
+            price: req.body.price,
+            img: req.body.img,
+            
+        };
+        
+       
+        fs.promises.readFile(FILE_PRODUCTOS).then(data => {
+
+            const json = JSON.parse(data.toString('utf-8'));
+            const producto=({...array, id:json.length +1});
+            const productoFinal=json.push(producto);
+            res.send(producto)
+            fs.promises.writeFile(FILE_PRODUCTOS, JSON.stringify(json, null, "\t"))
+            .then(() => {
+                console.log("Producto Agregado Correctamente");
+            })
+        }).then(data=>{
+            fs.promises.readFile(FILE_PRODUCTOS)
+            .then(data=>JSON.parse(data.toString('utf-8')))
+            .then(data=>res.json(data.length))
+            
+        })        
+});
+// METODOS PUT
+router.put('/productos/actualizar/:id',(req,res)=>{
+    var id=req.params.id;
+    console.log(id);
+    let actualizacion={
+        titulo: req.body.titulo,
+        price: req.body.price,
+        img: req.body.img,
+        
+    };
+
+
+
+    fs.promises.readFile(FILE_PRODUCTOS).then(data => {
+
+        const json = JSON.parse(data.toString('utf-8'));
+        const indexProduct=json.findIndex((obj)=>obj.id ==id);
+        json[indexProduct]={...actualizacion,id:json[indexProduct].id};
+        res.send(json[indexProduct])
+        fs.promises.writeFile(FILE_PRODUCTOS, JSON.stringify(json, null, "\t"))
+        .then(() => {
+            console.log("Producto Actualizado Correctamente");
+        })
+    })
+})
+router.delete('/productos/eliminar/:id',(req,res)=>{
+    var id=req.params.id;
+
+    fs.promises.readFile(FILE_PRODUCTOS).then(data => {
+
+        const json = JSON.parse(data.toString('utf-8'));
+        const filter=json.filter(data=>data.id!=id);
+        res.send(filter)
+        fs.promises.writeFile(FILE_PRODUCTOS, JSON.stringify(filter, null, "\t"))
+        .then(() => {
+            console.log("Producto Actualizado Correctamente");
+        })
+    })
+
+})
+module.exports=router;
